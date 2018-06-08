374d296a21f5cbe67c80fbe8c8337601402bd98e
kashstelio <39488544+kashstelio@users.noreply.github.com>
2018-06-08 14:59:58 +1000

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def available_states
+    [
+      ["New South Wales", "NSW"],
+      ["Vctoria", "VIC"],
+      ["Queensland" "QLD"],
+      ["Australian Capital Territory", "ACT"],
+      ["Western Australia", "WA"],
+      ["South Australia", "SA"],
+      ["Northern Territory", "NT"],
+      ["Tasmania", "TAS"]
+    ]
+  end
 end
