24a3c8d561348b8c0af3cf833b8eb990a015f03a
Kash Stelio <itsalot@Kashs-MBP.home.gateway>
2018-06-08 14:53:09 +1000

    fix

---
+++ app/assets/javascripts/spree/frontend/all.js
@@ -0,0 +1,17 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery_ujs
+//= require spree
+//= require spree/frontend
+//= require spree/frontend/checkout
+//= require spree/frontend/product
+//= require spree/frontend/cart
+//= require spree/frontend/orders
+//= require_tree .
+//= require solidus_paypal_braintree/frontend
+//= require spree/frontend/paypal_button
