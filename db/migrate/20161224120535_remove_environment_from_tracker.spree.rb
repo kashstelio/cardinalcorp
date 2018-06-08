4a59f3670b740aa6c8323c7d301cbdb4a8a6d1c4
kashstelio <39488544+kashstelio@users.noreply.github.com>
2018-06-08 10:47:54 +1000

    Update 20161224120535_remove_environment_from_tracker.spree.rb

--- db/migrate/20161224120535_remove_environment_from_tracker.spree.rb
+++ db/migrate/20161224120535_remove_environment_from_tracker.spree.rb
@@ -1,4 +1,7 @@
 # This migration comes from spree (originally 20150128032538)
+class Spree::Tracker < ApplicationRecord
+end
+
 class RemoveEnvironmentFromTracker < ActiveRecord::Migration[4.2]
   def up
     Spree::Tracker.where('environment != ?', Rails.env).update_all(active: false)
