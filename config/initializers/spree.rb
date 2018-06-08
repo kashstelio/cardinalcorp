42cacc370e6b869c4df5b638b63cdfec695dbec9
Yi Yang <zackyang214@gmail.com>
2018-06-08 22:25:47 +1000

    Disable production payment config

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -104,9 +104,9 @@ Spree::Config.configure do |config|
     SolidusPaypalBraintree::Gateway,
     'braintree_credentials', {
       environment: 'sandbox',
-      merchant_id: ENV['BRAINTREE_MERCHANT_ID'],
-      public_key: ENV['BRAINTREE_PUBLIC_KEY'],
-      private_key: ENV['BRAINTREE_PRIVATE_KEY']
+      merchant_id: '',#ENV['BRAINTREE_MERCHANT_ID'],
+      public_key: '',#ENV['BRAINTREE_PUBLIC_KEY'],
+      private_key: ''#ENV['BRAINTREE_PRIVATE_KEY']
     }
   )
 end
