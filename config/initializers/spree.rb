fc7abb09ac73ea9ecbabe3ca62140a517bec03a9
Yi Yang <zackyang214@gmail.com>
2018-06-08 22:02:37 +1000

    Set braintree to sandbox

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -103,7 +103,7 @@ Spree::Config.configure do |config|
   config.static_model_preferences.add(
     SolidusPaypalBraintree::Gateway,
     'braintree_credentials', {
-      environment: Rails.env.production? ? 'production' : 'sandbox',
+      environment: 'sandbox',
       merchant_id: ENV['BRAINTREE_MERCHANT_ID'],
       public_key: ENV['BRAINTREE_PUBLIC_KEY'],
       private_key: ENV['BRAINTREE_PRIVATE_KEY']
